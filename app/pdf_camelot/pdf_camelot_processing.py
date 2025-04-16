58e45f61e2a1abc5d0842138625627ad49e15e37
Sergey Dudnik <sdudnik05@mail.ru>
2025-04-16 23:45:30 +0700

    test bbox geo tables

--- app/pdf_camelot/pdf_camelot_processing.py
+++ app/pdf_camelot/pdf_camelot_processing.py
@@ -1,4 +1,16 @@
 import camelot
 
-tables = camelot.read_pdf('../pdfs/foo.pdf')
-print(tables[0].df)
+# tables = camelot.read_pdf('../pdfs/foo.pdf')
+# tables[0].to_json('foo.json')
+
+
+tables = camelot.read_pdf('../pdfs/foo.pdf', pages='1', flavor='lattice')  # или flavor='stream'
+
+if len(tables) > 0:
+    # Получение координат первой найденной таблицы
+    bbox = tables[0]._bbox
+    print(f"Координаты таблицы (x1, y1, x2, y2): {bbox}")
+    tables[0].to_json('bbox.json')
+
+else:
+    print("net")
